aa32eafde4a0f854cb40c908ca807fd74ed27529
arcatelier <aoooi522@outlook.jp>
2026-07-27 03:00:02 +0900

    アニメーション実装

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
 window.addEventListener("load", () => {
   document.body.classList.add("is-show");
 
+
+  /**
+   * ニュースのスリックの処理
+   */
+  $('.p-news__topics').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1
+  });
 })
