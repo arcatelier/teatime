17255c4c79ef6ed1070c308d8806b1060bdb63ce
arcatelier <aoooi522@outlook.jp>
2026-07-28 03:48:04 +0900

    スリック実装

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,19 @@
-window.addEventListener("load", () => {
-  document.body.classList.add("is-show");
-
-
-  /**
-   * ニュースのスリックの処理
-   */
+$(function () {
   $('.p-news__topics').slick({
     slidesToShow: 2,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    infinite: false,
+    arrows: true,
+    prevArrow: $('.js-prev'),
+    nextArrow: $('.js-next'),
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
-})
+});
